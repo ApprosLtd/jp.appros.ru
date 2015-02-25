b36f70743bdc8c7b701fffad2200f1b2d2a2f7a3
Vitaly Serov <serovvitaly@gmail.com>
2015-02-25 14:13:15 +0300

    1

--- app/Http/Controllers/Seller/SellerController.php
+++ app/Http/Controllers/Seller/SellerController.php
@@ -7,13 +7,21 @@ abstract class SellerController extends Controller {
 
     protected $layout = 'seller.layout';
 
+    protected function setupLayout()
+    {
+        if (!is_null($this->layout))
+        {
+            $this->layout = view($this->layout);
+        }
+    }
+
     public function callAction($method, $parameters)
     {
         $response = call_user_func_array(array($this, $method), $parameters);
 
         if (is_null($response) && !is_null($this->layout))
         {
-            $response = view($this->layout);
+            $response = $this->layout;
         }
 
         return $response;
