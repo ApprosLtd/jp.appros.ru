2a40bea896356aca8a01b4edb5ce0773545ab471
Vitaly Serov <serovvitaly@gmail.com>
2015-03-08 01:19:10 +0300

    работа над админкой

--- public/app/store/seller/Products/ProductsListStore.js
+++ public/app/store/seller/Products/ProductsListStore.js
@@ -2,13 +2,8 @@ Ext.define('App.store.seller.Products.ProductsListStore', {
     extend: 'Ext.data.Store',
     storeId:'sellerProductsListStore',
     autoLoad: true,
-    //autoSync: true,
+    autoSync: true,
     model: 'App.model.seller.Products.ProductsListModel',
-   /* fields: [
-        //{name: 'id',  type: 'int', useNull: true},
-        'firstName',
-        'age'
-    ],*/
     proxy: {
         type: 'rest',
         url: '/rest/products-list',
--- public/app/view/seller/Products/ProductEditWindow.js
+++ public/app/view/seller/Products/ProductEditWindow.js
@@ -7,9 +7,64 @@ Ext.define('App.view.seller.Products.ProductEditWindow', {
     height: 600,
     width: 800,
     autoShow: true,
+    constrain: true,
     modal: true,
     layout: 'fit',
     items: {
-
+        xtype: 'tabpanel',
+        items: [{
+            title: 'Главная',
+            xtype: 'panel',
+            items: {
+                xtype: 'form',
+                layout: 'fit',
+                //height: 500,
+                bodyPadding: 5,
+                border: false,
+                defaults: {
+                    //anchor: '100%'
+                },
+                items: [{
+                    fieldLabel: 'First Name',
+                    name: 'first',
+                    allowBlank: false
+                },{
+                    fieldLabel: 'Last Name',
+                    name: 'last',
+                    allowBlank: false
+                }]
+            }
+        },{
+            title: 'Атрибуты'
+        },{
+            title: 'Каталог'
+        },{
+            title: 'Цены'
+        },{
+            title: 'Фотогалерея'
+        }],
+        buttons: [{
+            text: 'Reset',
+            handler: function() {
+                this.up('form').getForm().reset();
+            }
+        }, {
+            text: 'Submit',
+            formBind: true, //only enabled once the form is valid
+            disabled: true,
+            handler: function() {
+                var form = this.up('form').getForm();
+                if (form.isValid()) {
+                    form.submit({
+                        success: function(form, action) {
+                            Ext.Msg.alert('Success', action.result.msg);
+                        },
+                        failure: function(form, action) {
+                            Ext.Msg.alert('Failed', action.result.msg);
+                        }
+                    });
+                }
+            }
+        }]
     }
 })
