b9b4a8894cf6abc389d8a5a3910dedd179f1071e
Vitaly Serov <serovvitaly@gmail.com>
2015-03-18 21:56:04 +0300

    адмика, роли пользователей

--- public/app/view/seller/Users/UserEditWindow.js
+++ public/app/view/seller/Users/UserEditWindow.js
@@ -61,38 +61,21 @@ Ext.define('App.view.seller.Users.UserEditWindow', {
             }]
         });
 
+        me.items = me.baseForm;
+
         if (me.record) {
             me.record.load({
                 success: function(record, operation){
-
                     var roles = record.get('roles');
                     if (roles.length) {
-                        var rolesValues = [];
                         Ext.Array.each(roles, function(role){
-                            rolesValues['roles['+role.id+']'] = true;
+                            record.set('roles['+role.id+']', true);
                         });
-                        me.baseForm.getForm().setValues(rolesValues);
                     }
-                    console.log(rolesValues);
-                    return;
-
-                    var fields = me.baseForm.getForm().getFields();
-
-                    fields.each(function(field){
-                        console.log(field.getName());
-                    });
-
-                    record.set('roles', {
-                        1:'on',
-                        2:1,
-                        3:1
-                    });
                     me.baseForm.getForm().loadRecord(record);
                 }
             });
         }
-
-        me.items = me.baseForm;
 
         me.dockedItems = [{
             dock: 'bottom',
